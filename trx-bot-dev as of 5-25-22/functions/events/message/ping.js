de92671d037b8b661b594349cb054495c574e64f
-Xen- <95078400+TobeXen@users.noreply.github.com>
2022-05-26 09:27:57 -0400

    Update ping.js

--- trx-bot-dev as of 5-25-22/functions/events/message/ping.js
+++ trx-bot-dev as of 5-25-22/functions/events/message/ping.js
@@ -5,6 +5,6 @@ if (context.params.event.content.startsWith(`${process.env.PREFIX}ping`)) {
   let latency = new Date() - new Date(context.params.event.timestamp);
   await lib.discord.channels['@0.1.1'].messages.create({
     channel_id: `${context.params.event.channel_id}`,
-    content: `<@${context.params.event.author.id}> 👾 (Time Taken: 0.${latency}s)`,
+    content: `👾 (Time Taken: 0.${latency}s)`,
   });
 }
