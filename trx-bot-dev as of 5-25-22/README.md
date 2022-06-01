490529ecbf6e22a761fca9b0b52844a6cd338d71
-Xen- <95078400+TobeXen@users.noreply.github.com>
2022-06-01 19:01:01 -0400

    Delete trx-bot-dev as of 5-25-22 directory

--- trx-bot-dev as of 5-25-22/README.md
+++
@@ -1,7 +0,0 @@
-# TRX Bot
-## Description:
-This is the code of TRX Bot made by Xen#9202 and exported from Autocode.
-Don't be mistaken by the name, this was generated or made from AI. 
-Autocode is just a service to run discord bot servers along with other things.
-
-If you need help either email trxproduction@yahoo.com or message the discord account Mr.Code#5130
--- trx-bot-dev as of 5-25-22/functions/events/message/bot-join.js
+++
@@ -1,8 +0,0 @@
-// authenticates you with the API standard library
-// type `await lib.` to display API autocomplete
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-
-await lib.discord.channels['@0.2.0'].messages.create({
-  channel_id: `${context.params.event.system_channel_id}`,
-  content: "TRX appreciates you inviting this bot, use ``t!help`` to see commands."
-});
